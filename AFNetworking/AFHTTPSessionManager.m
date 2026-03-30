cf12c80ab594d914704e640feafca48161118f1c
boringwind <boringwind@appest.com>
2026-03-30 21:31:24 +0800

    build project

--- AFNetworking/AFHTTPSessionManager.m
+++ AFNetworking/AFHTTPSessionManager.m
@@ -28,12 +28,6 @@
 #import <TargetConditionals.h>
 #import <Security/Security.h>
 
-#import <netinet/in.h>
-#import <netinet6/in6.h>
-#import <arpa/inet.h>
-#import <ifaddrs.h>
-#import <netdb.h>
-
 #if TARGET_OS_IOS || TARGET_OS_TV
 #import <UIKit/UIKit.h>
 #elif TARGET_OS_WATCH
